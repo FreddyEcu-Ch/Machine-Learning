266afa7cd98b408b959202e0848da7701a4f80e2
fredd <freddypcarrion@gmail.com>
2022-05-15 15:05:28 -0500

    Web app update

--- ml_app.py
+++ ml_app.py
@@ -14,7 +14,7 @@ from sklearn.tree import DecisionTreeClassifier
 
 # sns.set_style('darkgrid')
 
-# Page Layout
+#  Page Layout
 # Page expands to full width
 st.set_page_config(page_title="EOR_Ml App")
 
