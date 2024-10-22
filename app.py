31742078b17843255d7f20b5a60cbfbf9990beb5
FreddyEcu-Ch <fpcarrio@espol.edu.ec>
2024-10-22 12:45:06 -0500

    test 2

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title('Welcome to sotware for petroleum engineer')
+st.title("Welcome to sotware for petroleum engineer")
 
-st.write('This app is going to be used as the kickoff lecture of this subject')
-
+st.write("This app is going to be used as the kickoff lecture of this subject")
