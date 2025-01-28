6ad496fad11f023a62538eac430c38bdf832c0d7
FreddyEcu-Ch <fpcarrio@espol.edu.ec>
2025-01-28 09:26:30 -0500

    Update

--- ml_app.py
+++ ml_app.py
@@ -35,7 +35,7 @@ body {background-color: #DCE3D5;
 
 # Insert image
 logo = Image.open("Tyrmes Logo.png")
-st.image(logo, width=100, use_column_width=True)
+st.image(logo, width=100, use_container_width=True)
 
 # Adding of a mp4 video
 st.markdown(
